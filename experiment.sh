aaeb7c72e87e0af8f1d695d0292c9b072dddc4cc
Aditya Shukla <adityashukzy@gmail.com>
2025-11-10 02:38:52 -0500

    Change time limit to 20 hours

--- experiment.sh
+++ experiment.sh
@@ -4,7 +4,7 @@
 #SBATCH --gres=gpu:rtx_4090:1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=64G
-#SBATCH --time=02:00:00
+#SBATCH --time=20:00:00
 #SBATCH --output=slurm_logs/experiment_%j.out
 #SBATCH --error=slurm_logs/experiment_%j.err
 
